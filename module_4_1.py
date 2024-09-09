9917b803ccb24b4e6febaa9a897b8904065c33f6
FirstSage1 <voleynikov@yandex.ru>
2024-09-09 11:50:01 +0300

    Add files via upload

---
+++ module_4_1.py
@@ -0,0 +1,13 @@
+# module_4_1.py, fake_math и true_math
+# ======================================
+from fake_match import divide as fake_divide
+from true_match import divide as true_divide
+
+result1 = fake_divide(69, 3)
+result2 = fake_divide(3, 0)
+result3 = true_divide(49, 7)
+result4 = true_divide(15, 0)
+print(result1)
+print(result2)
+print(result3)
+print(result4)
